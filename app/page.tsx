e3d6eeb5f45896c71797a30a305a5b524ebaff06
Travis Luong <travis@travisluong.com>
2024-04-03 15:52:08 +0800

    dynamic home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
+import { getHeroText } from "@/lib/api";
 
-export default function Home() {
+export default async function Home() {
+  const heroText = await getHeroText();
+
+  console.log(heroText);
+
   return (
     <div>
       <Header />
       <main>
         <div className="h-[32rem] p-5 bg-[url(/images/hero.png)] bg-cover bg-center flex justify-end items-end">
           <div className="bg-black/50 text-white p-5 backdrop-brightness-50 text-3xl max-w-80">
-            <h1>Hero</h1>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+            <h1>{heroText.data.attributes.Title}</h1>
+            <p>{heroText.data.attributes.Content}</p>
           </div>
         </div>
         <div className="grid grid-cols-3 gap-5 p-5 max-w-screen-lg m-auto">
